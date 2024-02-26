3250f2b40fb885a05468d9e32d3bc16cde4d7234
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-02-26 22:33:53 +0900

    refactor -  application context 의존성 제거
    application context 에 의존되던 사용방식을 application.yaml 파일로 이동시킴
    따라서 설정들을 옮겨서 한번에 관리함

--- src/main/java/com/example/thymeleafexample/web/SpringWebConfig.java
+++ src/main/java/com/example/thymeleafexample/web/SpringWebConfig.java
@@ -2,50 +2,17 @@ package com.example.thymeleafexample.web;
 
 import com.example.thymeleafexample.web.conversion.DateFormatter;
 import com.example.thymeleafexample.web.conversion.VarietyFormatter;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring6.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
-@EnableWebMvc
-@ComponentScan
-public class SpringWebConfig implements WebMvcConfigurer, ApplicationContextAware {
-    private ApplicationContext applicationContext;
+public class SpringWebConfig implements WebMvcConfigurer {
 
     public SpringWebConfig() {
         super();
-    }
-
-    public void setApplicationContext(final ApplicationContext applicationContext)
-            throws BeansException {
-        this.applicationContext = applicationContext;
-    }
-
-    @Override
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        WebMvcConfigurer.super.addResourceHandlers(registry);
-        registry.addResourceHandler("/images/**").addResourceLocations("classpath:static/images/");
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:static/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:static/js/");
-    }
-
-    @Bean
-    public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        return messageSource;
     }
 
     @Override
@@ -66,29 +33,11 @@ public class SpringWebConfig implements WebMvcConfigurer, ApplicationContextAwar
     }
 
     @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setUseDecoupledLogic(true);
-        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("classpath:templates/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setCacheable(true);
-        return templateResolver;
-    }
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver
+    ) {
+        defaultTemplateResolver.setUseDecoupledLogic(true);
 
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        templateEngine.setEnableSpringELCompiler(true);
-        return templateEngine;
-    }
-
-    @Bean
-    public ThymeleafViewResolver viewResolver() {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(templateEngine());
-        return viewResolver;
+        return defaultTemplateResolver;
     }
 }
